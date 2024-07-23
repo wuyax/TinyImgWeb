f5aee48ed94f0055d5e10edf5468bbd4e7a935ae
wuyax <pengqingkai@gmail.com>
2024-07-23 19:13:47 +0800

    fix:优化显示

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from 'vue'
+import { computed } from 'vue'
 import { saveAs } from 'file-saver'
 import IconsLoading from '@/components/icons/IconLoading.vue'
 import IconImg from '@/components/icons/IconImg.vue'
+import IconWave from '@/components/icons/IconWave.vue'
 const props = defineProps<{ data: any }>()
 
 function save() {
@@ -23,7 +24,7 @@ const type = computed(() => {
   return props.data.type.split('/').pop().toUpperCase()
 })
 const ratio = computed(() => {
-  if(!props.data.resultSize){
+  if (!props.data.resultSize) {
     return '0'
   }
   return (((props.data.size - props.data.resultSize) / props.data.size) * -100).toFixed(2)
@@ -45,12 +46,17 @@ const ratio = computed(() => {
     <div>
       <div class="font-bold pb-1">{{ data.name }}</div>
       <div class="flex items-center">
-        <div class="text-[#039c93] bg-[#f2fafa] rounded-sm px-2 mr-2 font-bold">{{ type }}</div>
+        <div class="w-14 text-center text-[#039c93] bg-[#f2fafa] rounded-sm px-2 mr-2 font-bold">
+          {{ type }}
+        </div>
         <div>{{ formatFileSize(data.size) }}</div>
       </div>
     </div>
     <div class="ml-auto flex items-center">
-      <div class="flex flex-col justify-end items-end mr-2">
+      <div v-if="data.statu === 0" class="w-6 h-6 text-emerald-400 mr-2">
+        <IconWave class="w-full h-full"></IconWave>
+      </div>
+      <div v-else class="flex flex-col justify-end items-end mr-2">
         <div class="font-bold pb-1" :class="[!ratio.startsWith('-') ? 'text-red-600' : '']">
           {{ ratio }}%
         </div>
@@ -75,7 +81,7 @@ const ratio = computed(() => {
               d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 232V334.1l31-31c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-72 72c-9.4 9.4-24.6 9.4-33.9 0l-72-72c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l31 31V232c0-13.3 10.7-24 24-24s24 10.7 24 24z"></path>
           </svg>
         </div>
-        <div class="font-bold">{{ type }}</div>
+        <div class="w-10 font-bold">{{ type }}</div>
       </button>
     </div>
   </div>
